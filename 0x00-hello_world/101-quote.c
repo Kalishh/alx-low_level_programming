8881e382a0ed1030376b9e9cc7d820f33d09658f
Kalishh <kalishdebebe@gmail.com>
2022-09-11 12:06:22 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main -Entry pint
@@ -6,8 +7,13 @@
  * Return 0 alwasy (Success)
  */
 int main(void)
+
 {
-write("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
-return (1);	
+
+write(2,
+      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+return (1);
 
 }
