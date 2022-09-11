58cba73e222c3bbb255d7d912e69e753cc83a413
Kalishh <kalishdebebe@gmail.com>
2022-09-11 12:09:06 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,15 +4,15 @@
 /**
  * main -Entry pint
  *
- * Return 0 alwasy (Success)
+ * Return 0 Always (Success)
+ *
  */
+
 int main(void)
 
 {
 
-write(2,
-      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 return (1);
 
