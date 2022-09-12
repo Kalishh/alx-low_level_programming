684b0cd1ecbaa4e33d79d01ac0bda2cf5187eefc
Kalishh <kalishdebebe@gmail.com>
2022-09-12 06:36:11 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,23 +2,33 @@
 
 /**
 * main - Entry point
-*
+*  
 * Return: Always 0 (Success)
-* 
 */
+
 
 int main(void)
 
 {
+
 char abc;
+
 for (abc = 'a'; abc <= 'z'; abc++)
+
 {
+
 if (abc == 'q' || abc == 'e')
+
 {
+
 continue;
+
 }
+
 putchar(abc);
+
 }
+
 putchar('\n');
 
 return (0);
