6bebf58cf898d2ebf22e19e68686ba881454c861
Kalishh <kalishdebebe@gmail.com>
2022-09-12 09:09:16 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -10,12 +9,9 @@
 int main(void)
 
 {
-
 	int nums;
 
 	int nums1;
-
-	int nums2;
 
 
 	for (nums = '0'; nums <= '7'; nums++)
@@ -25,17 +21,11 @@ int main(void)
 	for (nums1 = nums + 1; nums1 <= '9'; nums1++)
 
 	{
-
-	for (nums2 = nums1 + 1; nums2 <= '9'; nums2++)
-
-	{
 		putchar(nums);
 
 		putchar(nums1);
 
-		putchar(nums2);
-
-	if (nums == '7' && nums1 == '8' && nums2 == '9')
+	if (nums == '7' && nums1 == '8')
 		break;
 
 	putchar(',');
@@ -43,8 +33,6 @@ int main(void)
 	putchar(' ');
 
 	}
-	}
-
 	}
 
 	putchar('\n');
