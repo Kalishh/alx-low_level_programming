19ed2461cfb4a790c7d9baa76429a09a086dfba5
Kalishh <kalishdebebe@gmail.com>
2022-09-12 03:48:34 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char smallLetters;
+
+	for(smallLetters = 'a'; smallLetters <= 'z'; smallLetters++);
+		putchar(smallLetters);
+
+	putchar('\n');
+
+	return (0);
+
+
+}
