31cae8055e2b0dc5f12a2ecd5b88c2184b9504d0
Kalishh <kalishdebebe@gmail.com>
2022-09-13 11:56:51 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - using this function 
+ * Return; 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+
+char ltr = 0 , ltrs;
+
+while(ltr<10)
+{
+	ltrs = 'a';
+	while(ltrs<= 'z')
+	{
+	_putchar(ltrs);
+	ltrs++;
+}
+_putchar('\n');
+ltr++;
+}
+}
