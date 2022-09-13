3b31ca06e05557d4658fa3f4254e125d93fcd34d
Kalishh <kalishdebebe@gmail.com>
2022-09-13 10:18:00 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code
- * Return: Always 0.
- */
+* print_alphabet - check the code
+* Return: Always 0.
+*/
 
 void print_alphabet(void)
 
 {
        char a;
 for (a = 'a'; a <= 'z'; a++)
-	{	
+{	
 _putchar(a);
-	}	
+}	
 _putchar('\n');
 }
