6875499ff3cfa7df66fa46b56cdc97a7a628bc9f
Kalishh <kalishdebebe@gmail.com>
2022-09-13 09:27:52 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * 
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+       int a;
+	for (a = 'A'; a <= 'Z', a++) 
+	print_alphabet(a);
+
+	        return (0);
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,6 +20,8 @@ int mul(int a, int b);
 
 void print_numbers(void);
 
+void print_alphabet(void);
+
 void print_most_numbers(void);
 
 void more_numbers(void);
