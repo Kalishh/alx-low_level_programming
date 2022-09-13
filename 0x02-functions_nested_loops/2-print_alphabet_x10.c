919d9b79c07ba4d6a0d62bba2a30d7b6c247e86f
Kalishh <kalishdebebe@gmail.com>
2022-09-13 12:35:13 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,17 @@
 
 /**
 * print_alphabet_x10 - using this function 
+*
 * Return; 0 (Success)
 */
 
 void print_alphabet_x10(void)
 {
-char ltr = 0 , ltrs;
-while(ltr<10)
+char ltr = 0, ltrs;
+while (ltr < 10)
 {
 ltrs = 'a';
-	while(ltrs<= 'z')
+	while (ltrs <= 'z')
 {
 	_putchar(ltrs);
 	ltrs++;
