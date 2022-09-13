0139065f409b7792108387a40dd743942cad7a3c
Kalishh <kalishdebebe@gmail.com>
2022-09-13 08:49:47 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,9 +11,18 @@
  * Return; 0 Always (Success)
  */
 
-int main()
+int main(void)
 
 {
-	write(1, "_putchar \n", 1);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
