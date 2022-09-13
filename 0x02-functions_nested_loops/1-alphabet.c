b403c02e2fe8c2aa5997f20a87019579c0ca8c60
Kalishh <kalishdebebe@gmail.com>
2022-09-13 10:12:31 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,6 @@
 
 /**
  * main - check the code
- * 
  * Return: Always 0.
  */
 
@@ -10,10 +9,9 @@ void print_alphabet(void)
 
 {
        char a;
-	for (a = 'a'; a <= 'z'; a++)
+for (a = 'a'; a <= 'z'; a++)
 	{	
-	_putchar(a);
-	}
-	
-	_putchar('\n');
+_putchar(a);
+	}	
+_putchar('\n');
 }
