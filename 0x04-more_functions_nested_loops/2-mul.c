ac57e2fe36bb22d34e8775ba01f85ead57393deb
Kalishh <kalishdebebe@gmail.com>
2022-09-15 13:38:09 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * int mul(int a, int b) - function used
+ * @a: is a variable
+ * @b: is variable
+ * Return: multipied value
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
