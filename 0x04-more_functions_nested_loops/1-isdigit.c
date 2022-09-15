fcf7588f7025354cb2c7465f11e752dfdc51e036
Kalishh <kalishdebebe@gmail.com>
2022-09-15 12:56:35 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - is the function used
+ * @c: is the variable used
+ * Return: 0 and 1
+ */
+
+int _isdigit(int c)
+{
+	for(c = 0; c <= 9; c++)
+{
+	printf ("%d\n", c);
+	return (1);
+}
+return (0);
+}
