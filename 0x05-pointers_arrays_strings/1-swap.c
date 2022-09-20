af1ee0aa64edd2c253ad4de4c578632326544e17
Kalishh <kalishdebebe@gmail.com>
2022-09-20 13:33:58 -0700

    1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,13 +4,11 @@
  * swap_int - swaps two numbers
  * @a: first variable
  * @b: second vatiable
- * @tmp: where the no is stored temporarily
  */
 
 void swap_int(int *a, int *b)
 {
 	int tmp;
-	
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
