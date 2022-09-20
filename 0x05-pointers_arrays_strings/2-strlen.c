7bd9f18512b14843f8507d660c4b30fb229463d0
Kalishh <kalishdebebe@gmail.com>
2022-09-20 13:47:01 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - string length
+ * @s - string
+ * Return: largest 
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+}
+
