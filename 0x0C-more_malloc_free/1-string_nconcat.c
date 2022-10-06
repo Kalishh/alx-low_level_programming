6645639e76464a6e40b28926f7d4ce715cf0df5a
Kalishh <kalishdebebe@gmail.com>
2022-10-06 13:14:56 -0700

    3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,29 +1,45 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _strdup - copy the string given to a newly allocated space in memory
- * @str: string to copy
- * Return: returns a pointer to the copy, or NULL if it fails
+ * string_nconcat - concat two strings to n bytes
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to concat of string 2
+ * Return: pointer to concatenated string or NULL if it fails
  */
 
-char *_strdup(char *str)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int x, len;
-	
-	
-	if (!str)
-		return (NULL);
-	
-	for (len = 0; str[len]; len++)
+	unsigned int i, len1, len2;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+
+	for (len1 = 0; *(s1 + len1); len1++)
 		;
-	len++;
-	ptr = malloc(len * sizeof(char));
+	for (len2 = 0; *(s2 + len2); len2++)
+		;
 
-	if (!ptr)									return (NULL);
+	if (len2 < n)
+		n = len2;
 
-	for (x = 0; x <= len; x++)
-		ptr[x] = str[x];
-										return (ptr);
+	ptr = malloc((len1 + n + 1) * sizeof(char));
+
+	if (!ptr)
+		return (NULL);
+
+	for (i = 0; i < (len1 + n); i++)
+	{
+		if (i < len1)
+			ptr[i] = s1[i];
+		else
+			ptr[i] = s2[i - len1];
+	}
+	ptr[i] = '\0';
+
+	return (ptr);
 }
